4555599d5c8beda53e365e9b17f49a5d1f201e07
CodeforNitin <codeforni3@gmail.com>
2024-03-20 03:33:08 +0530

    final touch to css

--- accordian/script.js
+++ accordian/script.js
@@ -9,9 +9,12 @@ accordians.forEach(accordian => {
         
         if(icon.classList.contains('active')) {
             icon.classList.remove('active');
+            accordian.classList.remove('active');
             answer.style.maxHeight = null;
+
         } else {
             icon.classList.add('active');
+            accordian.classList.add('active');
             answer.style.maxHeight = answer.scrollHeight + 'px';
         }
         
