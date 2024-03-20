04d31c318df1a7e50a973c32a43370d49c2615ae
CodeforNitin <codeforni3@gmail.com>
2024-03-21 02:33:48 +0530

    added logic for repetitve switc mode

--- bulb-switch/script.js
+++ bulb-switch/script.js
@@ -1,22 +1,25 @@
-const btns = document.querySelectorAll('.btn');
-const image = document.querySelector('#image')
+    const btns = document.querySelectorAll('.btn');
+    const image = document.querySelector('#image')
 
-btns.forEach(function(button){
+    let isOn = false;
 
-    button.addEventListener('click', function(e){
-        if (e.currentTarget.classList.contains('on')){
-            document.body.style.backgroundColor = "azure";
-            image.src="images/bulb-on.png";
-            image.classList.toggle("img-off");
-            image.classList.toggle("img-on");
+    btns.forEach(function(button){
 
-        }
+        button.addEventListener('click', function(e){
+            if (e.currentTarget.classList.contains('on') && isOn == false){
+                document.body.style.backgroundColor = "azure";
+                image.src="images/bulb-on.png";
+                image.classList.toggle("img-off");
+                image.classList.toggle("img-on");
+                isOn=true;
+            }
 
-        else{
-            document.body.style.backgroundColor = "black";
-            image.src = "images/bulb-off.png";
-            image.classList.toggle("img-off");
-            image.classList.toggle("img-on");
-        }
+            else if (e.currentTarget.classList.contains('off') && isOn == true) {
+                document.body.style.backgroundColor = "black";
+                image.src = "images/bulb-off.png";
+                image.classList.toggle("img-off");
+                image.classList.toggle("img-on");
+                isOn=false;
+            }
+        });
     });
-});
